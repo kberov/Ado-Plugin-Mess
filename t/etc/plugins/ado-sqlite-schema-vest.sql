e13f93a5cad237818fc6c81a8243f3e113f38f4c
Krasimir Berov <k.berov@gmail.com>
2014-09-11 15:07:22 +0200

    renaming

--- t/etc/plugins/ado-sqlite-schema-vest.sql
+++ t/etc/plugins/ado-sqlite-schema-vest.sql
@@ -1,33 +1,43 @@
 /**
-  This SQL script creates the table mess in an 
+  This SQL script creates the table vest in an 
   existing ado.sqlite database.
-  The following pragmas should be set by DSC plugin
+  The following pragmas must be already set by DSC plugin
   PRAGMA encoding = "UTF-8"; 
   PRAGMA foreign_keys = ON;
 */
 
 -- 'This table stores the messages between users'
-CREATE TABLE IF NOT EXISTS mess (
+CREATE TABLE IF NOT EXISTS vest (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  -- A user id from which the message is sent. 
   from_uid INT(11) REFERENCES users(id) NOT NULL,
   -- A user id to which the message is sent. 
-  -- Can be a user group which has a record in the users table.
+  -- Can be zero '0' in case the message is sent to the whole group.
   -- This way we can have group talks.
-  to_uid INT(11) REFERENCES users(id) NOT NULL,
-  -- Subject of the talk. Only the first message in a talk has a subject
+  to_uid INT(11) REFERENCES users(id) NOT NULL DEFAULT 0,
+  -- A group id to which the message is sent.
+  -- Can be zero '0'. In this case the message is private to the user specified
+  -- by to_uid. In case both to_uid and to_guid are zero, the sender 
+  -- is talking to him self - Taking Notes.
+  to_guid INT(11) REFERENCES groups(id) NOT NULL DEFAULT 0,
+  -- Subject of the talk. Only the first message in a talk has a subject.
   subject VARCHAR(255) NOT NULL DEFAULT '',
   -- Id of the first message in a talk. Every next message has this !=0.
-  subject_message_id INT(12) NOT NULL DEFAULT 0,
-  --  'last modification time'
-  --  'All dates are stored as seconds since the epoch(1970) in GMT. In Perl we use gmtime as object from Time::Piece'
+  -- This way a conversation can have a topic.
+  -- The first message in a talk sets the topic.
+  -- There can be many conversations in a group or between two users.
+  subject_vest_id INT(12) NOT NULL DEFAULT 0,
+  -- Last modification time.
+  -- All dates are stored as seconds since the epoch(1970). 
+  -- In Perl we use a Time::Piece object.
   tstamp INTEGER NOT NULL DEFAULT 0,
-  -- the message it self
+  -- The message it self.
   message TEXT,
   -- File-names of Files attached to this message - TODO
   message_assets TEXT DEFAULT NULL,
   -- Who can edit this message - usually only the owner.
   permissions VARCHAR(10) NOT NULL DEFAULT '-rw-r-----' 
 );
-CREATE INDEX IF NOT EXISTS mess_subject ON mess(subject);
-CREATE INDEX IF NOT EXISTS mess_subject_message_id ON mess(subject_message_id);
+CREATE INDEX IF NOT EXISTS vest_subject ON vest(subject);
+CREATE INDEX IF NOT EXISTS vest_subject_vest_id ON vest(subject_vest_id);
 
